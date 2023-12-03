dc552d84094110610dff2df464c27b6f84112514
Ramon Lopes <ramonlindo2311@gmail.com>
2023-12-03 02:51:32 -0300

    testing

--- teste.js
+++ teste.js
@@ -1,4 +1,12 @@
-console.log('hello world');
-console.log('new test for push');
-console.log('teste switch branch');
-console.log('teste switch for third branch');
+// console.log('hello world');
+// console.log('new test for push');
+// console.log('teste switch branch');
+// console.log('teste switch for third branch');
+
+let age = 22;
+let canIDrink = 18;
+if (age >= canIDrink) {
+  console.log('yes you can ');
+} else {
+  console.log('no you can not ');
+}
