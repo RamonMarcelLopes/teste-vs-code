587a6df54c3873323c16638bb4eba8e96efcda94
Ramon Lopes <ramonlindo2311@gmail.com>
2023-12-09 01:44:08 -0300

    feat add interface for play teh game

---
+++ script.js
@@ -0,0 +1,75 @@
+let button = document.getElementById('button-action');
+let select = document.getElementById('types');
+let input = document.getElementById('input-age');
+let img = document.getElementById('loading-img');
+let textResult = document.getElementById('result');
+let yourage = document.getElementById('your-age');
+
+///variables///
+let minAge = 18;
+let age = 0;
+let choice = 'current';
+let results = Boolean;
+let currentAgeForShow = 0;
+
+//////////////
+/////////////
+///functions////
+
+let selectChange = () => {
+  choice = select.value;
+};
+let inputChanged = () => {
+  age = input.value;
+};
+let setResult = () => {
+  setInterval(() => {
+    img.classList.remove('loading-img-true');
+    img.classList.add('loading-img-false');
+
+    if (results == true) {
+      textResult.innerHTML = ' ';
+      textResult.innerHTML = 'The result is YES you can ';
+      yourage.innerHTML = `you are ${currentAgeForShow} years old`;
+    }
+    if (results == false) {
+      textResult.innerHTML = '';
+      textResult.innerHTML = 'The result is No you can NOT ';
+      yourage.innerHTML = `you are ${currentAgeForShow} years old`;
+    }
+  }, 2000);
+};
+
+let buttonPressed = () => {
+  textResult.innerHTML = ' ';
+  yourage.innerHTML = ``;
+  img.classList.remove('loading-img-false');
+  img.classList.add('loading-img-true');
+  if (choice == 'current') {
+    if (age >= minAge) {
+      results = true;
+      currentAgeForShow = age;
+    } else {
+      results = false;
+      currentAgeForShow = age;
+    }
+  }
+  if (choice == 'noCurrent') {
+    let today = new Date();
+    let year = today.getFullYear();
+    let test = year - age;
+    currentAgeForShow = test;
+    if (test >= minAge) {
+      results = true;
+    } else {
+      results = false;
+    }
+  }
+
+  setResult();
+};
+/////////////
+
+button.addEventListener('click', buttonPressed);
+select.addEventListener('change', selectChange);
+input.addEventListener('change', inputChanged);
