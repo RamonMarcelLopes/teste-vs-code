53398b630b2501536c294e7a78acc6b9cebb02b7
Ramon Lopes <ramonlindo2311@gmail.com>
2023-12-08 22:03:51 -0300

    fix game fo response number 1

--- teste.js
+++ teste.js
@@ -1,13 +1,10 @@
 const ask = require('prompt-sync')();
-// console.log('hello world');
-// console.log('new test for push');
-// console.log('teste switch branch');
-// console.log('teste switch for third branch');
-// let question1 = ask(
-//   'if you want say your year of birth type 1 | if you want write you current age write 2  '
-// );
+
 const canIDrink = 18;
 let age = 0;
+
+console.log('lets see if you can drink some beers');
+
 let question1 = ask(
   'if you want say your year of birth type 1 | if you want write you current age write 2  '
 );
@@ -19,14 +16,18 @@ while (question1 >= 3) {
 }
 
 if (question1 == 1) {
-  age = ask('write year of birth ');
+  let response = ask('write year of birth ');
+  let today = new Date();
+  let year = today.getFullYear();
+  let responseset = year - response;
+  age = responseset;
 }
 if (question1 == 2) {
   age = ask('write your current age ');
 }
 
 if (age >= canIDrink) {
-  console.log('yes you can ');
+  console.log(`you are ${age} yes you can `);
 } else {
-  console.log('no you can not ');
+  console.log(`you are ${age} no you can not `);
 }
